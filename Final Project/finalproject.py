2769ec0ab76887700a4092bf6c93cd635b8c7cec
Alexandre Acra <alexandre.acra@yahoo.com>
2022-05-06 11:49:27 -0700

    finished creating ML model

--- Final Project/finalproject.py
+++ Final Project/finalproject.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
@@ -12,9 +11,8 @@ crimes = crimes[["REPORTING_AREA","Lat","Long","HOUR","DAY_OF_WEEK","OFFENSE_COD
 crimes.replace(" ",np.nan,inplace=True)
 codes = pd.read_csv('bostoncrime/offense_codes.csv',encoding="latin1")
 crimes.dropna(inplace=True)
-print(crimes)
 
-def predict_crime(area,latitude,longitude,hour,dayofweek):
+def predict_crime():
     feature_area = crimes["REPORTING_AREA"].values
     feature_latitude = crimes["Lat"].values
     feature_longitude = crimes["Long"].values
@@ -34,18 +32,34 @@ def predict_crime(area,latitude,longitude,hour,dayofweek):
 
     feattrain, feattest, classtrain, classtest = train_test_split(features, class_offense, test_size=0.2)
 
-    model = KNeighborsClassifier(n_neighbors=2).fit(feattrain, classtrain)
+    model = KNeighborsClassifier(n_neighbors=24).fit(feattrain, classtrain)
+    classpred = model.predict(feattest)
 
+    print("Accuracy: ", accuracy_score(classtest, classpred))
+
+
+def predict_crime_user(area,latitude,longitude,hour,dayofweek):
+    feature_area = crimes["REPORTING_AREA"].values
+    feature_latitude = crimes["Lat"].values
+    feature_longitude = crimes["Long"].values
+    feature_hour = crimes["HOUR"].values
+    feature_dayofweek = crimes["DAY_OF_WEEK"].values
+    class_offense = crimes[["OFFENSE_CODE"]].values
+
+    day_transformer = LabelEncoder().fit(feature_dayofweek)
+    feature_dayofweek = day_transformer.transform(feature_dayofweek)
+    dayofweek = day_transformer.transform([dayofweek])[0]
+
+    features = np.array([feature_area, feature_latitude, feature_longitude, feature_hour, feature_dayofweek]).transpose()
+
+    scaler = StandardScaler().fit(features)
+    features = scaler.transform(features)
+
+    feattrain, feattest, classtrain, classtest = train_test_split(features, class_offense, test_size=0.2)
+
+    model = KNeighborsClassifier(n_neighbors=2).fit(feattrain, classtrain)
     value = model.predict([[area,latitude,longitude,hour,dayofweek]])
     print(value)
 
-    # print("Accuracy: ", accuracy_score(classtest, classpred))
-    #
-    # cm = confusion_matrix(classtest, classpred, labels=class_labels)
-    # cmd = ConfusionMatrixDisplay(cm, display_labels=class_labels)
-    # cmd.plot()
-
-
-# def predict_crime(latitude, longitude, hour, dayofweek): # checking for redundancy of area with physical location
-
-predict_crime(808,42.357,-71.14,13,"Sunday")
+predict_crime()
+# predict_crime_user(417,42.282,-71.073,12,"Tuesday")
